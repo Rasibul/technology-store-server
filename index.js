538043090e8e5003beb8081683f684d816cf2cd0
Rasibul <rasibul179@gmail.com>
2023-10-20 23:39:37 +0600

    Create And Read Opeartion

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const express = require("express")
 const cors = require("cors")
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 
 // midelware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xmelfag.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db('productDB').collection("product")
+
+    app.post('/products', async (req, res) => {
+      const product = req.body
+      console.log(product)
+      const result = await productCollection.insertOne(product)
+      res.send(result)
+  })
+
+  app.get('/:name', async (req, res) => {
+    const brand = req.params.name.toLowerCase()
+    const query = { brand: brand }
+    const result = await productCollection.find(query).toArray()
+    res.send(result)
+})
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send("techonolgy surver is running")
